60c835c19db68de04704a483167c5e694d4cd83e
william_gilbert <William.Gilbert@nuance.com>
2016-06-03 13:45:33 -0400

    Use PowerMock to write JUnit test for GWT Random class which has a JavaScript native implementation

---
+++ 1-HelloWorld-Simple/src/test/java/demo/client/GwtRandomTest.java
@@ -0,0 +1,59 @@
+
+package demo.client;
+
+import com.google.gwt.user.client.Random;
+
+// JUnit 4.x testing
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+// PowerMock
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( { Random.class })
+
+/**
+* If not correctly mocked, this class will not compile
+*
+*/
+
+public class GwtRandomTest  {
+
+    @Before
+    public void createStaticMocks() throws Throwable {
+
+        // Mock Singleton
+        mockStatic(Random.class);
+
+        // Use JDK Random class in place of GWT Random class
+        java.util.Random jdkRandom = new java.util.Random();
+        when(Random.nextInt()).thenReturn(jdkRandom.nextInt());
+        when(Random.nextDouble()).thenReturn(jdkRandom.nextDouble());
+    }
+
+    @Test
+    public void randomInt() {
+        int random = Random.nextInt();
+    }
+
+    @Test
+    public void randomDouble() {
+        double random = Random.nextDouble();
+    }
+
+  
+
+}
